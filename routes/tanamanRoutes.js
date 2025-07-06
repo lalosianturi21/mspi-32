e026726068f34cdc90ec4bd599a6468cb7333eab
lalosianturi21 <fulalotio@gmail.com>
2025-07-06 22:37:21 +0700

    update get all tanaman data

--- routes/tanamanRoutes.js
+++ routes/tanamanRoutes.js
@@ -1,13 +1,17 @@
 import express from "express";
 const router = express.Router();
 import { adminGuard, authGuard } from "../middleware/authMiddleware.js";
-import { createTanaman, deleteTanaman, getAllTanaman, getSingleTanaman, updateTanaman } from "../controllers/tanamanController.js";
+import { createTanaman, deleteTanaman, getAllTanaman, getAllTanamanData, getSingleTanaman, updateTanaman } from "../controllers/tanamanController.js";
 
 
 router
     .route("/")
     .post(authGuard, adminGuard, createTanaman)
     .get(getAllTanaman);
+
+router
+    .route("/alltanaman")
+    .get(getAllTanamanData);
 
 router
     .route("/:tanamanId")
